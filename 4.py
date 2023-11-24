36583ee8f67f6e3025bdb7fbdcda6dd7fd50dc90
P1tchZ3r0 <106891834+P1tchZ3r0@users.noreply.github.com>
2023-11-24 18:59:22 +1000

    3

--- 4.py
+++ 4.py
@@ -1,9 +1,9 @@
 import math
-x = int(input())
+x = abs(int(input('Введите значение x: ')))
 a = x // 1000
 b = x % 1000 // 100
 c = x % 100 // 10
 d = x % 10
-sum=(math.cos(a)+math.cos(b)+math.cos(c)+math.cos(d))
-print(a,b,c,d)
-print(sum)
+sum = (math.cos(a)+math.cos(b)+math.cos(c)+math.cos(d))
+print('Цифры введенного числа x:',a,b,c,d)
+print('Сумма косинусов цифр числа x: ',sum)
