ab7361bb24b22657504f318cb130c9e24fa66240
P1tchZ3r0 <106891834+P1tchZ3r0@users.noreply.github.com>
2024-01-14 13:24:19 +1000

    Паскаль
    1

---
+++ 1.pas
@@ -0,0 +1,29 @@
+﻿Program v1;
+type
+student = record
+name:string[10];
+group:integer;
+ball:array [1..5] of 2..5;
+srb:real
+end;
+var
+  st:array[1..10] of student;
+  i,j,d:integer;
+begin
+  for i:=1 to 10 do
+  begin
+    writeln('Введите фамилию студента');
+    readln(st[i].name);
+    writeln('Введите номер его группы');
+    readln(st[i].group);
+    writeln('Введите 5 оценок');
+    for j:=1 to 5 do
+      readln(st[i].ball[j]);
+   d:=0;
+   for j:=1 to 5 do d:=d +st[i].ball[j];
+   st[i].srb:=d/5;
+   end;
+ for i:=1 to 10 do
+   if st[i].srb>=4 then writeln(st[i].name)
+end.
+
---
+++ 2.pas
@@ -0,0 +1,16 @@
+﻿var many:set of char;
+     stroka:string; i:byte;
+ begin
+   write('Введите строку: ');
+   readln(stroka);
+   many := [];
+   i:=1;
+   while i<= length(stroka) do
+     if stroka[i] in many
+       then delete(stroka,i,1)
+       else begin
+         many:=many+[stroka[i]];
+         inc(i);
+       end;
+       writeln(stroka);
+ end.
